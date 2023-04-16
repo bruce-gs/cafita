c1f66e59feeb6e60dd3bed6b2337aeb2be80d136
bruce-gs <83456120+bruce-gs@users.noreply.github.com>
2023-04-16 15:17:52 -0500

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const config = require('./config);
-const { Client } = require('discord.js');
-const client = new Client({ intents: 3276799 });
-
-client.login(config.token).then(console.log(`Bot conectado/a como ${client.user.username}`));
