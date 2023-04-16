0f7a29457822fb381f8886fb7268e13995676af5
bruce-gs <83456120+bruce-gs@users.noreply.github.com>
2023-04-16 13:55:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
+const config = require('./config);
 const { Client } = require('discord.js');
 const client = new Client({ intents: 3276799 });
 
-client.login();
+client.login(config.token).then(console.log(`Bot conectado/a como ${client.user.username}`));
